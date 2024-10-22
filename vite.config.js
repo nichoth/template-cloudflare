0f9eea3b9d39879e00eafcccf7e318d9bbd8f08a
nichoth <nichoth@nichoth.com>
2024-10-22 10:46:19 -0700

    add more cloudflare

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig({
     },
     build: {
         target: 'esnext',
-        minify: false,
+        minify: 'terser',
         outDir: './public',
         emptyOutDir: true,
         sourcemap: 'inline'
